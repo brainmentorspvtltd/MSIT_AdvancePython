5a0845979bc7d380314a3846b5c4f8cc7e5af99c
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-06-17 15:08:54 +0530

    Add files via upload

--- OnlineShop/cgi-bin/index.py
+++ OnlineShop/cgi-bin/index.py
@@ -2,7 +2,7 @@
 products = [
     {
         "p_id": 101,
-        "p_category": "Kitchen Appliances",
+        "p_category": "Kitchen_Appliances",
         "p_name": "2 SLICE SANDWICH MAKER Grill  (Black & Steel)",
         "brand": "Nova",
         "price": 1695,
@@ -10,7 +10,7 @@ products = [
     },
     {
         "p_id": 102,
-        "p_category": "Kitchen Appliances",
+        "p_category": "Kitchen_Appliances",
         "p_name": "Favourite IC 1800 W Induction Cooktop  (Black, Push Button)",
         "brand": "Pigeon",
         "price": 1399,
@@ -18,7 +18,7 @@ products = [
     },
     {
         "p_id": 103,
-        "p_category": "Home Appliances",
+        "p_category": "Home_Appliances",
         "p_name": "Ujala 1200 mm Energy Saving 3 Blade Ceiling Fan  (Brown, Pack of 1)",
         "brand": "Orient Electric",
         "price": 1399,
@@ -26,15 +26,15 @@ products = [
     },
     {
         "p_id": 104,
-        "p_category": "Kitchen Appliances",
+        "p_category": "Kitchen_Appliances",
         "p_name": "HL1655/00 250 W Hand Blender  (White)",
         "brand": "Philips",
         "price": 1550,
-        "image": "https://rukminim1.flixcart.com/image/416/416/k0y6cnk0/hand-blender/m/s/f/philips-hl1655-00-hl1655-00-original-imafkmkfuzwyrg5m.jpeg?q=70"
+        "image": "https://images-na.ssl-images-amazon.com/images/I/41-5nd-r6bL._SX425_.jpg"
     },
     {
         "p_id": 105,
-        "p_category": "Kitchen Appliances",
+        "p_category": "Kitchen_Appliances",
         "p_name": "Rapid 4 Jar 750 watts 750 W Juicer Mixer Grinder  (Black, 4 Jars)",
         "brand": "Butterfly",
         "price": 2799,
@@ -42,7 +42,7 @@ products = [
     },
     {
         "p_id": 106,
-        "p_category": "Kitchen Appliances",
+        "p_category": "Kitchen_Appliances",
         "p_name": "HR7627/00 650 W Food Processor  (White)",
         "brand": "Philips",
         "price": 4250,
@@ -50,7 +50,7 @@ products = [
     },
     {
         "p_id": 107,
-        "p_category": "Water Purifiers",
+        "p_category": "Water_Purifiers",
         "p_name": "Ace Plus 8 L RO + UV + UF + TDS Water Purifier  (White)",
         "brand": "Kent",
         "price": 12999,
@@ -58,7 +58,7 @@ products = [
     },
     {
         "p_id": 108,
-        "p_category": "Water Purifiers",
+        "p_category": "Water_Purifiers",
         "p_name": "LIV-PEP-PRO-PLUS+ 7 L RO + UV + UF Water Purifier  (White)",
         "brand": "Livpure",
         "price": 9299,
@@ -66,7 +66,7 @@ products = [
     },
     {
         "p_id": 109,
-        "p_category": "Home Appliances",
+        "p_category": "Home_Appliances",
         "p_name": "Oeh-1220 OEH-1220 Fan Room Heater",
         "brand": "Orpat",
         "price": 1079,
@@ -74,7 +74,7 @@ products = [
     },
     {
         "p_id": 110,
-        "p_category": "Air Conditioners",
+        "p_category": "Window_Air_Conditioners",
         "p_name": "1.5 Ton 3 Star Window AC - White  (183 DZA (R32), Copper Condenser)",
         "brand": "Voltas",
         "price": 23999,
@@ -82,15 +82,15 @@ products = [
     },
     {
         "p_id": 111,
-        "p_category": "Air Conditioners",
-        "p_name": "1.5 Ton 3 Star Split Inverter AC with PM 2.5 Filter - White  (18K 3 Star Ester Neo Inverter R32 IDU (I011) / 18K 3Star Inverter R32 ODU (I011), Copper Condenser)",
+        "p_category": "Air_Conditioners",
+        "p_name": "1.5 Ton 3 Star Split Inverter AC with PM 2.5 Filter - White  (18K 3 Star Ester Neo Inverter, Copper Condenser)",
         "brand": "Carrier",
         "price": 30499,
         "image": "https://rukminim1.flixcart.com/image/416/416/jtyouq80/air-conditioner-new/s/f/8/18k-3-star-ester-neo-inverter-r32-idu-i011-18k-3star-inverter-original-imaff727rpzg3nhz.jpeg?q=70"
     },
     {
         "p_id": 112,
-        "p_category": "Air Conditioners",
+        "p_category": "Air_Conditioners",
         "p_name": "1.2 Ton 5 Star Split Inverter AC - White  (155V DZW (R32), Copper Condenser)",
         "brand": "Voltas",
         "price": 31499,
@@ -103,10 +103,19 @@ print('''
 <head>
     <title>Title</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="../design.css">
+    <style>
+      .card-img-top{
+        align-self: center;
+      }
+      .card img{
+        width: 210px;
+      }
+    </style>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <a class="navbar-brand" href="#">Online Shop</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -116,10 +125,10 @@ print('''
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">All Products</a>
       </li>
-      <li class="nav-item dropdown">
+      <li class="nav-item dropdown active">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown
         </a>
@@ -130,8 +139,8 @@ print('''
           <a class="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">My Cart</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
@@ -140,6 +149,32 @@ print('''
     </form>
   </div>
 </nav>
+''')
+
+print('''
+  <div class="container">
+    <h1 class="text-center">Products</h1>
+    <hr>
+    <div class="row">
+''')
+
+for product in products:
+    print(f'''
+    <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+    <div class="card {product["p_category"]}" style="width: 95%; height: 33rem; margin-bottom: 20px; padding: 10px">
+      <img src="{product["image"]}" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">{product["brand"]} {product["p_name"]}</h5>
+        <p class="card-text">Price : {product["price"]}</p>
+        <a href="#" class="btn btn-primary">Add To Cart</a>
+      </div>
+    </div>
+    </div>
+  ''')
+
+print('''
+    </div>
+  </div>
 ''')
 
 
